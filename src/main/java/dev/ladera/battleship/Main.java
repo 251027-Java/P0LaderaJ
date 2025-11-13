4bfa81d965465a84b70a74749579df4f68347b95
JB Ladera <minidomou@gmail.com>
2025-11-13 15:46:44 -0600

    add and configure logging

--- src/main/java/dev/ladera/battleship/Main.java
+++ src/main/java/dev/ladera/battleship/Main.java
@@ -1,7 +1,13 @@
 package dev.ladera.battleship;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     static void main() {
+        LOGGER.info("Main started");
         System.out.println("hello");
     }
 }
